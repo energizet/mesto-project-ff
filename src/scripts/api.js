1910148c9d5d667225ba1869a40617cc9fc5d36d
energizet <a2437@yandex.ru>
2024-05-29 15:48:28 +0300

    add UpdateProfile

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,16 +1,53 @@
-export function GetMe() {
+export function GetProfile() {
     return fetch(`${process.env.BASE_URL}/users/me`, {
         headers: {
             authorization: process.env.API_TOKEN,
         },
-    }).then(d => d.json());
-}
+    }).then(res => {
+        if (res.ok) {
+            return res.json();
+        }
 
+        return Promise.reject(`Ошибка: ${res.status}`);
+    }).catch(err => {
+        console.log(err);
+        return {};
+    });
+}
 
 export function GetCards() {
     return fetch(`${process.env.BASE_URL}/cards`, {
         headers: {
             authorization: process.env.API_TOKEN
         }
-    }).then(d => d.json());
+    }).then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+
+        return Promise.reject(`Ошибка: ${res.status}`);
+    }).catch(err => {
+        console.log(err);
+        return [];
+    });
+}
+
+export function UpdateProfile(profile) {
+    return fetch(`${process.env.BASE_URL}/users/me`, {
+        method: 'PATCH',
+        headers: {
+            authorization: process.env.API_TOKEN,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: profile.name,
+            about: profile.about,
+        })
+    }).then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+
+        return Promise.reject(`Ошибка: ${res.status}`);
+    }).catch(err => console.log(err));
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,7 @@ import '../pages/index.css';
 import {like, createCard, removeCard} from "./components/card";
 import {openPopupFabric} from "./components/modal";
 import {clearValidation, enableValidation} from "./validation";
-import {GetCards, GetMe} from './api';
+import {GetCards, GetProfile, UpdateProfile} from './api';
 
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
@@ -30,14 +30,16 @@ const validationConfig = {
     errorClass: 'popup__error_visible'
 };
 
-const [me, cards] = await Promise.all([
-    GetMe(),
+const [profile, cards] = await Promise.all([
+    GetProfile(),
     GetCards(),
 ]);
 
-profileTitle.textContent = me.name;
-profileDescription.textContent = me.about;
-profileImage.style.backgroundImage = `url('${me.avatar}')`;
+profile._id = 'c85011613b4e17f9e8a92fe9';
+
+profileTitle.textContent = profile.name;
+profileDescription.textContent = profile.about;
+profileImage.style.backgroundImage = `url('${profile.avatar}')`;
 
 placesList.append(...cards.map(item => createCardProxy(item)));
 
@@ -47,8 +49,8 @@ registerAddPopup();
 enableValidation(validationConfig);
 
 function createCardProxy(cardData) {
-    cardData.isLike = cardData.likes.some(user => user._id === me._id);
-    cardData.isCanDelete = cardData.owner._id === me._id;
+    cardData.isLike = cardData.likes.some(user => user._id === profile._id);
+    cardData.isCanDelete = cardData.owner._id === profile._id;
 
     return createCard(cardTemplate, cardData, {removeCard, like, openCard});
 }
@@ -62,13 +64,12 @@ function openCard(cardData) {
 }
 
 function registerEditPopup() {
-    const profileTitle = document.querySelector('.profile__title');
-    const profileDescription = document.querySelector('.profile__description');
-
     const profileEdit = document.querySelector('.profile__edit-button');
 
     const popupProfileTitle = popupProfileEdit.querySelector('.popup__input_type_name');
     const popupProfileDescription = popupProfileEdit.querySelector('.popup__input_type_description');
+    const popupProfileButton = popupProfileEdit.querySelector('.popup__button');
+    const buttonDefaultText = popupProfileButton.textContent;
 
     const popupForm = popupProfileEdit.querySelector('.popup__form');
 
@@ -76,16 +77,31 @@ function registerEditPopup() {
     popupForm.addEventListener('submit', e => submitForm(e, submit, closePopup));
 
     profileEdit.addEventListener('click', () => {
-        popupProfileTitle.value = profileTitle.textContent;
-        popupProfileDescription.value = profileDescription.textContent;
+        popupProfileTitle.value = profile.name;
+        popupProfileDescription.value = profile.about;
 
         clearValidation(popupProfileEdit, validationConfig);
         closePopup = openPopupEdit();
     });
 
-    function submit() {
-        profileTitle.textContent = popupProfileTitle.value;
-        profileDescription.textContent = popupProfileDescription.value;
+    async function submit() {
+        popupProfileButton.textContent = 'Сохранение...';
+
+        const updatedProfile = await UpdateProfile({
+            name: popupProfileTitle.value,
+            about: popupProfileDescription.value,
+        });
+
+        popupProfileButton.textContent = buttonDefaultText;
+
+        if (updatedProfile == null) {
+            return;
+        }
+
+        Object.assign(profile, updatedProfile);
+
+        profileTitle.textContent = profile.name;
+        profileDescription.textContent = profile.about;
     }
 }
 
@@ -95,6 +111,8 @@ function registerAddPopup() {
 
     const cardName = popupProfileAdd.querySelector('.popup__input_type_card-name');
     const cardUrl = popupProfileAdd.querySelector('.popup__input_type_url');
+    const cardButton = popupProfileEdit.querySelector('.popup__button');
+    const buttonDefaultText = cardButton.textContent;
 
     const popupForm = popupProfileAdd.querySelector('.popup__form');
 
@@ -108,6 +126,11 @@ function registerAddPopup() {
     });
 
     function submit() {
+        cardButton.textContent = 'Сохранение...';
+
+
+        cardButton.textContent = buttonDefaultText;
+
         placesList.prepend(createCardProxy({
             name: cardName.value,
             link: cardUrl.value,
@@ -115,9 +138,9 @@ function registerAddPopup() {
     }
 }
 
-function submitForm(e, submit, closePopup) {
+async function submitForm(e, submit, closePopup) {
     e.preventDefault();
-    submit();
+    await submit();
 
     if (closePopup instanceof Function) {
         closePopup();
